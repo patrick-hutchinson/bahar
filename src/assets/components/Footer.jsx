d8db5d5545b3800004f793e84a2b1257961f0ffa
patrick-hutchinson <hutchinsonpatrick@icloud.com>
2025-04-08 13:40:28 +0200

    Update Footer.jsx

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -14,7 +14,7 @@ export default function Footer() {
 
         <div className="contact">
           <div className="cell">CELL {data.telephone}</div>
-
+          <br />
           <a
             href="https://www.facebook.com/profile.php?id=61573744333759&mibextid=wwXIfr&rdid=AzR06cyUTr4HDgWK&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F15q2AUYVhT%2F%3Fmibextid%3DwwXIfr"
             target="_blank"
@@ -22,6 +22,7 @@ export default function Footer() {
           >
             FB {data.facebook}
           </a>
+          <br />
           <a href={`https://www.instagram.com/${data.instagram}`} target="_blank" className="instagram">
             IG {data.instagram}
           </a>
